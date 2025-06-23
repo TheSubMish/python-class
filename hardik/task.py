2f5c1c463a9bb1beefcae62db1d48561e488b71a
hardikmahato <hardikmahato@gmail.com>
2025-06-23 14:35:18 +0545

    Task operation

---
+++ hardik/task.py
@@ -0,0 +1,33 @@
+def view():
+    with open("task.txt") as t:
+        global read
+        read= t.readlines()
+    for i in range(len(read)):
+        print(f"\tTask {i+1} - {read[i]}")
+
+while True:
+    operation = int(input("1.ADD TASK\n2.VIEW TASKS\n3.DELETE TASK\n4.EXIT\nWhich one:"))
+
+    match operation:
+
+        case 1:
+            taskName = input("Enter a task:")
+            with open("task.txt","a") as t:
+                t.write(f"{taskName}\n")
+
+        case 2:
+            view()
+
+        case 3:
+            i = int(input("DELETE TASK: "))
+            with open("task.txt","w") as t:
+                for final in read:
+                    if final != read[i-1]:
+                        t.write(final)
+      
+        case 4:
+            print("EXITING")
+            break
+
+        case _:
+            print("Invalid Input")
