6115cbf1f6596a3b08a9aa6f3c5b161f727fd44b
hardikmahato <hardikmahato@gmail.com>
2025-06-23 17:28:48 +0545

    added todo program

--- hardik/task.py
+++ hardik/task.py
@@ -2,16 +2,16 @@ def view():
     with open("task.txt") as t:
         global read
         read= t.readlines()
+    print("VIEWING ALL TASKS:")
     for i in range(len(read)):
         print(f"\tTask {i+1} - {read[i]}")
 
 while True:
-    operation = int(input("1.ADD TASK\n2.VIEW TASKS\n3.DELETE TASK\n4.EXIT\nWhich one:"))
+    operation = int(input("\nOPTOINS:\n1.ADD TASK\n2.VIEW TASKS\n3.DELETE TASK\n4.EXIT\n\tCHOOSE:"))
 
     match operation:
-
         case 1:
-            taskName = input("Enter a task:")
+            taskName = input("Enter a task:") 
             with open("task.txt","a") as t:
                 t.write(f"{taskName}\n")
 
@@ -19,6 +19,7 @@ while True:
             view()
 
         case 3:
+            view()
             i = int(input("DELETE TASK: "))
             with open("task.txt","w") as t:
                 for final in read:
