b98186389449a106215e54c0d4be08899c02fde6
TheSubMish <subodhmishra015@gmail.com>
2025-06-30 11:50:12 +0545

    class

--- class/django/first_project/app/models.py
+++ class/django/first_project/app/models.py
@@ -5,6 +5,7 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField()
+    address = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.age}) - {self.email}"
