4d89f3af4fc7d0ba15a91b612efc018b283dff81
TheSubMish <subodhmishra015@gmail.com>
2025-07-20 09:46:42 +0545

    class

---
+++ class/django/ecommerce/auth/utils.py
@@ -0,0 +1,16 @@
+import os
+from django.core.exceptions import ValidationError
+
+def image_validate(image):
+    check_exts = (".jpg", ".jpeg", ".png")
+    file_extension= os.path.splitext(str(image))
+    if image.size >= 1000000:
+        raise ValidationError("File should less than 1MB")
+    if file_extension[1] == check_exts[0] or file_extension[1] == check_exts[1] or file_extension[1] == check_exts[2]:
+        pass
+    else:
+        raise ValidationError("Provide Valid Image file such as jpeg, jpg, png")
+    if image:
+        pass
+    else:
+        raise ValidationError("Image is not provided")
